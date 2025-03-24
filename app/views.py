d8f83b2aa6d6f25a9b9322c16d7eb45adf9e047d
joshhhhh1234 <joshuabarnido27@gmail.com>
2025-03-24 17:05:56 +0800

    inventory.html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 class DashboardPageView(TemplateView):
     template_name = 'app/dashboard.html'
 
@@ -15,5 +16,7 @@ class ReportPageView(TemplateView):
 
 class SettingsPageView(TemplateView):
     template_name = 'app/settings.html'
+
+    
 
 # Create your views here.
